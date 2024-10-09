43d3c8643901bf246adbd9e430702874107beb44
Amir Hamza <a.hamza8596@gmail.com>
2024-10-08 23:59:53 -0400

    Removed image classification/updated NLP and OCR logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,32 @@
 "use client";
 import Tesseract from "tesseract.js";
 import axios from "axios";
-import { useState} from "react";
-
+import { useState } from "react";
 
 export default function Home() {
   const [image, setImage] = useState(null);
   const [text, setText] = useState("");
   const [fileName, setFileName] = useState("");
-  const [predictions, setPredictions] = useState([]);
-
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
-    setImage(URL.createObjectURL(file));
-    setFileName(file.name.toLowerCase());
+    if (file) {
+      setImage(URL.createObjectURL(file));
+      setFileName(file.name.toLowerCase());
 
+      if (
+        !file.name.toLowerCase().includes("xray") &&
+        !file.name.toLowerCase().includes("x-ray")
+      ) {
+        performOCR(file);
+      }
+    } else {
+      console.log("No file selected");
+    }
+  };
+
+  const performOCR = (file) => {
+    console.log(file);
     // Perform OCR on document images
     Tesseract.recognize(file, "eng", { logger: (m) => console.log(m) }).then(
       ({ data: { text } }) => {
@@ -23,7 +34,6 @@ export default function Home() {
       }
     );
   };
-
 
   const handleNLP = async () => {
     try {
@@ -49,50 +59,12 @@ export default function Home() {
     }
   };
 
-  const handleAIClassification = async () => {
-    try {
-      const response = await axios.post(
-        "https://api.openai.com/v1/chat/completions",
-        {
-          model: "gpt-3.5-turbo",
-          messages: [
-            { role: "system", content: `Describe the following medical image: ${fileName}` },
-          ],
-          max_tokens: 100,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-          },
-        }
-      );
-      setText(response.data.choices[0].message.content);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
   return (
     <div>
-      <h1>Healthcare Demo</h1>
+      <h1>POC</h1>
       <input type="file" onChange={handleFileChange} />
-      {/*Classify only X-ray or CT images*/}
-      {image && (
-        <img
-          id="Image"
-          src={image}
-          alt="Image"
-          width={300}
-          onLoad={
-            fileName.includes("xray") || fileName.includes("ct")
-              ? handleAIClassification
-              : null
-          }
-        />
-      )}
+      {image && <img id="Image" src={image} alt="ig" width={300} />}
       <button onClick={handleNLP}>Extract Medical Terms</button>
-      {/* <button onClick={handleAIClassification}>Classify Image</button> */}
-
       <p>{text}</p>
     </div>
   );
